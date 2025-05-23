ce3203e5b12cd710ecc069a21c6b85f7e9d87bec
Олексій <gavrilov.lucky@gmail.com>
2025-05-23 17:35:21 +0300

    hw-js-09

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,30 +1,32 @@
   
-import '../css/styles.css';  
+import '../css/styles.css';
 import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const galleryContainer = document.querySelector('.gallery');
+document.addEventListener('DOMContentLoaded', () => {
+  const galleryContainer = document.querySelector('.gallery');
 
-const galleryMarkup = galleryItems
-  .map(
-    ({ preview, original, description }) => `
-    <li class="gallery-item">
-      <a class="gallery-link" href="${original}">
-        <img
-          class="gallery-image"
-          src="${preview}"
-          alt="${description}"
-        />
-      </a>
-    </li>
-  `
-  )
-  .join('');
+  if (!galleryContainer) {
+        return;
+  }
 
-galleryContainer.innerHTML = galleryMarkup;
+  const galleryMarkup = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${original}">
+            <img class="gallery-image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `
+    )
+    .join('');
 
-new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
+  galleryContainer.innerHTML = galleryMarkup;
+
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 });
